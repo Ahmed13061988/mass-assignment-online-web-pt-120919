9400aea63963a92ca58fe3c47356745ea66165b2
Ahmed Hussein <wolktheline@yahoo.com>
2020-01-23 22:29:25 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,6 +3,6 @@ class Person
 :weight, :handed, :complexion, :t_shirt_size, 
 :wrist_size, :glove_size, :pant_length, :pant_width 
 
- def initialize(name:,birthday:,hair_color:,eye_color:)
+ def initialize(name:,birthday:,hair_color:,eye_color:,height:,weight:,handed:,complexion:,t_shirt_size:,wrist_size:,glove_size:,pant_lenght:,pant_width:)
  end 
 end
